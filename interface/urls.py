1614242a870ae13b06d3546e8542b25dd5bc4f13
liyongda <liyongda@zcool.com.cn>
2021-07-23 18:22:34 +0800

    day 2

--- interface/urls.py
+++ interface/urls.py
@@ -4,12 +4,18 @@ from . import views
 
 app_name = 'interface'
 urlpatterns = [
-    # /interface
-    path('', views.index, name='index'),
-    # /interface/5/
-    path('<int:tag_id>/', views.detail, name='detail'),
-    # /interface/5/results/
-    path('<int:tag_id>/results/', views.results, name='results'),
-    # /interface/5/vote
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetaiView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultView.as_view(), name='results'),
     path('<int:tag_id>/vote/', views.vote, name='vote'),
+
+
+    # # /interface
+    # path('', views.index, name='index'),
+    # # /interface/5/
+    # path('<int:tag_id>/', views.detail, name='detail'),
+    # # /interface/5/results/
+    # path('<int:tag_id>/results/', views.results, name='results'),
+    # # /interface/5/vote
+    # path('<int:tag_id>/vote/', views.vote, name='vote'),
 ]
--- interface/views.py
+++ interface/views.py
@@ -2,48 +2,67 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
+from django.views import generic
 
 from .models import Tags, Ttttt
 
 
-# Create your views here.
-def index(request):
-    latest_question_list = Tags.objects.order_by('-create_at')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'interface/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'interface/index.html'
+    context_object_name = 'latest_question_list'
 
-    # latest_question_list = Tags.objects.order_by('-create_at')[:5]
-    # template = loader.get_template('interface/index.html')
-    # context = {
-    #     'latest_question_list': latest_question_list,
-    # }
-    # return HttpResponse(template.render(context, request))
-
-    # output = ', '.join([q.name for q in latest_question_list])
-    # return HttpResponse(output)
-    # return HttpResponse("You'redsxc
-    # looking at tag %s." )
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Tags.objects.order_by('-create_at')[:5]
 
 
-def detail(request, tag_id):
-    question = get_object_or_404(Tags, pk=tag_id)
-    return render(request, 'interface/detail.html', {'question': question})
-
-    # return HttpResponse("You're looking at tag %s." % tag_id)
-    # try:
-    #     question = Tags.objects.get(pk=tag_id)
-    # except Tags.DoesNotExist:
-    #     raise Http404("Tags does not exist")
-    # return render(request, 'interface/detail.html', {'question': question})
+class DetaiView(generic.DetailView):
+    model = Tags
+    template_name = 'interface/detail.html'
 
 
-def results(request, tag_id):
-    # response ="You're looking at the results of tag %s."
-    # return HttpResponse(response % tag_id)
-    question = get_object_or_404(Tags, pk=tag_id)
-    # return render(request, 'interface/results.html', {'question': question})
-    return render(request, 'interface/results.html', {'question': question})
+class ResultView(generic.DetailView):
+    model = Tags
+    template_name = 'interface/results.html'
 
+
+# # Create your views here.
+# def index(request):
+#     latest_question_list = Tags.objects.order_by('-create_at')[:5]
+#     context = {'latest_question_list': latest_question_list}
+#     return render(request, 'interface/index.html', context)
+#
+#     # latest_question_list = Tags.objects.order_by('-create_at')[:5]
+#     # template = loader.get_template('interface/index.html')
+#     # context = {
+#     #     'latest_question_list': latest_question_list,
+#     # }
+#     # return HttpResponse(template.render(context, request))
+#
+#     # output = ', '.join([q.name for q in latest_question_list])
+#     # return HttpResponse(output)
+#     # return HttpResponse("You'redsxc
+#     # looking at tag %s." )
+#
+#
+# def detail(request, tag_id):
+#     question = get_object_or_404(Tags, pk=tag_id)
+#     return render(request, 'interface/detail.html', {'question': question})
+#
+#     # return HttpResponse("You're looking at tag %s." % tag_id)
+#     # try:
+#     #     question = Tags.objects.get(pk=tag_id)
+#     # except Tags.DoesNotExist:
+#     #     raise Http404("Tags does not exist")
+#     # return render(request, 'interface/detail.html', {'question': question})
+#
+#
+# def results(request, tag_id):
+#     # response ="You're looking at the results of tag %s."
+#     # return HttpResponse(response % tag_id)
+#     question = get_object_or_404(Tags, pk=tag_id)
+#     # return render(request, 'interface/results.html', {'question': question})
+#     return render(request, 'interface/results.html', {'question': question})
 
 
 def vote(request, tag_id):
